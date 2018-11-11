20f03ba3037e377093b59d3922dedd9d4dbde614
sven <sven@gwelt.net>
2018-11-11 22:48:22 +0100

    some display art

--- ESP32-Cube.ino
+++ ESP32-Cube.ino
@@ -1,67 +1,61 @@
 #include <WiFi.h>
-#include "secret.h"
-const char* ssid     = SECRET_SSID;
-const char* password = SECRET_WLANPWD;
+#include "config.h"
+const char* ssid     = SSID;
+const char* password = PWD;
 WiFiServer server(80);
-IPAddress local_IP(192, 168, 1, 110);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
+byte myIP[] = IP;
+IPAddress local_IP(myIP[0],myIP[1],myIP[2],myIP[3]);
+byte myGW[] = GATEWAY;
+IPAddress gateway(myGW[0],myGW[1],myGW[2],myGW[3]);
+byte mySN[] = SUBNET;
+IPAddress subnet(mySN[0],mySN[1],mySN[2],mySN[3]);
 
-#include "DHT.h"
-#define DHTPIN 23     // what digital pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#include <DHT.h>
+#define DHTPIN 23
+#define DHTTYPE DHT22 // DHT 22 (AM2302), AM2321
 DHT dht(DHTPIN, DHTTYPE);
 
 #include <TM1637Display.h>
-#define CLK 19 // GRUEN
-#define DIO 21 // GELB
+#define CLK 19
+#define DIO 21
 TM1637Display display(CLK, DIO);
 
-int t = 0;
-unsigned long time2;
-int counter = 0;
-
-String default_res="<a href=\"/\">ESP32</a><br>Click <a href=\"/H\">here</a> to turn the LED on.<br>Click <a href=\"/L\">here</a> to turn the LED off.<br><br>TEMP: ";
-int stepms=5000;
-
-void blink(); 
+unsigned long timeflag = 0; // millis at last update
+int counter = 0; // current counter
+int temp = 0; // current temperature
+int number=0; // current number on display
+int stepms=5000; // ms to wait between updates
+int art_z=0;
+int art_d=0;
+unsigned long art_timestamp=0;
+byte art_data[] = { 0b00000001, 0b00000010, 0b00000100, 0b00001000 };
 
 void setup()
 {
-		Serial.begin(115200);
-		pinMode(2, OUTPUT);
-		display.setBrightness(0x00, true);
-		display.showNumberDecEx(local_IP[3], 0b00000000, false, 4, 0);
-		//uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
-		//display.setSegments(data);
-		blink(5,50);
-		if (!WiFi.config(local_IP, gateway, subnet)) {Serial.println("STA Failed to configure");}
-		Serial.print("Connecting to ");
-		Serial.print(ssid);
-		WiFi.begin(ssid, password);
-		while (WiFi.status() != WL_CONNECTED) {delay(500); Serial.print(".");}
-		Serial.print("WiFi connected. ");
-		Serial.print("IP address: ");
-		Serial.println(WiFi.localIP());
-		blink(1,800);
-
-		server.begin();
-		dht.begin();
+	Serial.begin(115200);
+	pinMode(2, OUTPUT);
+	blink(2,50);
+	display.setBrightness(0x00, true);
+	uint8_t data[] = { 0b01001001, 0b01001001, 0b01001001, 0b01001001 };
+	display.setSegments(data);
+	if (!WiFi.config(local_IP, gateway, subnet)) {Serial.println("STA Failed to configure");}
+	Serial.print("Connecting to ");
+	Serial.print(ssid);
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED) {delay(500); Serial.print(".");}
+	Serial.print("WiFi connected. IP address: ");
+	Serial.println(WiFi.localIP());
+	display.showNumberDecEx(myIP[3], 0b00000000, false, 4, 0);
+	blink(1,800);
+	server.begin();
+	dht.begin();
 }
 
 void loop(){
+	art(0,0);
+	if (abs(millis()-timeflag)>stepms) {timeflag = millis(); temp=read_dht22(); if (temp>0&&temp<10000) {updateDisplay(temp);};}
+
 	WiFiClient client = server.available();
-
-	if (abs(millis()-time2)>stepms)
-	{
-		int number=counter;
-		time2 = millis();
-		int t_new=read_dht22();
-		if (t_new<10000) {t=t_new;}
-		if (t>0) {number=t;}
-		display.showNumberDec(number, false, 4);
-	}
-
 	if (client) {                         
 		String currentLine = "";
 		String res = "";
@@ -85,11 +79,12 @@ void loop(){
 
 				} else if (c != '\r') {
 					currentLine += c;
-					if (currentLine.equals("GET / ")) {display.showNumberDec(++counter, false, 4); res=default_res+String(t);}
-					else if (currentLine.equals("GET /H ")) {digitalWrite(2, HIGH); display.showNumberDec(++counter, false, 4); res=default_res+String(t);}
-					else if (currentLine.equals("GET /L ")) {digitalWrite(2, LOW); display.showNumberDec(++counter, false, 4); res=default_res+String(t);}
-					else if (currentLine.equals("GET /ON ")) {display.setBrightness(0x00, true); res=default_res+String(t);}
-					else if (currentLine.equals("GET /OFF ")) {display.setBrightness(0x00, false); res=default_res+String(t);}
+					if (currentLine.equals("GET / ")) {res=assambleRES();}
+					else if (currentLine.equals("GET /H ")) {digitalWrite(2, HIGH); res=assambleRES();}
+					else if (currentLine.equals("GET /L ")) {digitalWrite(2, LOW); res=assambleRES();}
+					else if (currentLine.equals("GET /ON ")) {display.setBrightness(0x00, true); res=assambleRES();}
+					else if (currentLine.equals("GET /OFF ")) {display.setBrightness(0x00, false); res=assambleRES();}
+					else if (currentLine.equals("GET /ART ")) {res=assambleRES(); art(48,80);}
 				}
 			}
 		}
@@ -97,6 +92,33 @@ void loop(){
 	}
 }
 
+String assambleRES() {
+	++counter;
+	art(12,40);
+	return "<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head><body style=font-size:2em><a href=\"/\">ESP32</a><br>Click <a href=\"/H\">here</a> to turn the LED on.<br>Click <a href=\"/L\">here</a> to turn the LED off.<br><br>TEMP: "
+	+String(temp)
+	+"<br>COUNTER: "
+	+String(counter)
+	+"</body></html>";
+}
+
+void art(int z, int d) {
+	if (z>0||d>0) {art_z=z; art_d=d;} 
+	else if (art_z>0) {
+		Serial.print('.');
+		if (abs(millis()-art_timestamp)>art_d) {
+			art_z--;
+			art_timestamp=millis();
+			for (int a=0; a < 4; a++) {
+				art_data[a]=art_data[a]<<1;
+				if (art_data[a]==0b01000000) {art_data[a]=0b00000001;}
+			}
+			display.setSegments(art_data);
+			Serial.println(art_z);
+			if (art_z<=0) {updateDisplay(counter);}
+		}
+	}
+}
 
 void blink(int z, int d) {
 	for (int i=0; i < z; i++){
@@ -108,8 +130,11 @@ void blink(int z, int d) {
 }
 
 int read_dht22() {
-	//float h = dht.readHumidity();
 	float t = dht.readTemperature();
-	//float f = dht.readTemperature(true);
+	//float h = dht.readHumidity();
 	return int(t*100);
+}
+
+void updateDisplay(int n) {
+	display.showNumberDec(n, false, 4);
 }
---
+++ sample_config.h
@@ -0,0 +1,5 @@
+#define SSID "MyNetWorkName"
+#define PWD "mysecretpassword"
+#define IP { 192, 168, 1, 100 }
+#define GATEWAY { 192, 168, 1, 1 }
+#define SUBNET { 255, 255, 255, 0 }
