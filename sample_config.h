954a44a7da3f16130f7d46d330de66eb553c9f8c
sven <sven@gwelt.net>
2018-11-25 23:04:42 +0100

    AP and STA WiFi sample_config

--- sample_config.h
+++ sample_config.h
@@ -1,8 +1,3 @@
-#define SSID {"WLAN1","WLAN2"}
-#define PWD {"password1","password2"}
-#define NUMBER_OF_SSIDS 2
-#define IP { 0, 0, 0, 0 } // 0,0,0,0 = DHCP, last byte of IP will be displayed on TM1637Display
-#define GATEWAY { 192, 168, 1, 1 }
-#define SUBNET { 255, 255, 255, 0 }
+#define AP_SSID "8.8.8.8"
 #define SOCKETIOHOST "192.168.1.111"
 #define SOCKETIOPORT 3000
